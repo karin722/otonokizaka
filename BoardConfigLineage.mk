d74e396942dc2180917e269c0baabb315a36dc83
karin722 <fubdonstrk@gmail.com>
2019-11-23 14:17:01 +0900

    up

---
+++ BoardConfigLineage.mk
@@ -0,0 +1,14 @@
+# Common board config for crosshatch, blueline
+
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := b1c1_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/google/crosshatch
+
+-include vendor/google/crosshatch/BoardConfigVendor.mk
