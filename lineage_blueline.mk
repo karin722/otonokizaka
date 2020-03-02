2f262e054eac22e9397ab72a1ef80f5974bbff3c
karin722 <fubdonstrk@gmail.com>
2020-03-02 19:07:29 +0900

    add

---
+++ lineage_blueline.mk
@@ -0,0 +1,30 @@
+GAPPS_VARIANT := stock
+GAPPS_FORCE_PACKAGE_OVERRIDES := true
+GAPPS_BYPASS_PACKAGE_OVERRIDES := Youtube
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2160
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
+
+-include device/google/crosshatch/blueline/device-lineage.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_blueline
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 3
+TARGET_MANUFACTURER := Google
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=blueline \
+    PRIVATE_BUILD_DESC="blueline-user 9 PQ3A.190801.002 5670241 release-keys"
+
+BUILD_FINGERPRINT := google/blueline/blueline:9/PQ3A.190801.002/5670241:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
+$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
